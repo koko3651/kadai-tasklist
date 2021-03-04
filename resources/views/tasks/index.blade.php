5122fa36739130ad238cb086481f4132f8314fb2
koki.makino <koko3651koko@gmail.com>
2021-03-04 07:59:31 +0000

    add index.blade.php

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -26,6 +26,6 @@
         </table>
     @endif
     {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
