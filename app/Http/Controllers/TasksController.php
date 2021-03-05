34336c8abd5d60c9618769e63bed4d459dd571d6
koki.makino <koko3651koko@gmail.com>
2021-03-05 00:37:31 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -52,11 +52,13 @@ class TasksController extends Controller
     {
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',   
             'content' => 'required|max:255',
         ]);
         
         // メッセージを作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -112,11 +114,13 @@ class TasksController extends Controller
     {
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',
             'content' => 'required|max:255',
         ]);
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
